1db351e2a8540045d166155259e14a1044e0f99a
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-12 10:41:01 -0500

    estructura

--- proyecto17/src/app/app-routing.module.ts
+++ proyecto17/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BreakComponent } from './pages/break/break.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"home", component:HomeComponent },
+  {path:"nosotros", component:NosotrosComponent },
+  {path:"contacto", component:ContactoComponent },
+  {path:"404", component:BreakComponent},
+  {path:"**", redirectTo:"404", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
